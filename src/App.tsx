13e621ffb59cdf9b960281c942e72e3159f4b552
Feiagop <iagoefy@gmail.com>
2022-03-01 19:08:40 -0300

    admin funcitons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
@@ -10,10 +13,13 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rooms/new" element={<NewRoom />} />
-        </Routes>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,55 @@
-import { useNavigate } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { database } from '../services/firebase';
+
 import { Button } from '../components/Button';
-import { useAuht } from '../hooks/useAuth';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 
+
+// Criar sala
 export function Home() {
-  const navigate = useNavigate();
-  const { user, signInWithGoogle } = useAuht()
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth()
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreatRoom() {
     if (!user) {
       await signInWithGoogle()
     }
 
-    navigate('/rooms/new');
+    history.push('/rooms/new');
 
+  }
+  
+  //entrar em uma sala
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    if (roomRef.val().endedAt) {
+      alert ('Room already closed.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -38,15 +67,16 @@ export function Home() {
               Crie sua sala com o Google
             </button>
             <div className="separator">ou entre em uma sala</div>
-            <form>
+            <form onSubmit={handleJoinRoom}>
               <input 
               type="text"
               placeholder="Digite o código da sala"
-               />
-               <Button type="submit">
-                 Entrar na sala
-
-               </Button>
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
+              />
+              <Button type="submit">
+                Entrar na sala
+              </Button>
             </form>
           </div>
         </main>
